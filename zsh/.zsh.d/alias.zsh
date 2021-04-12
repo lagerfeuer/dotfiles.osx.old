5567d1ff37370780584bb3ffda3da23b10b048d4
Lukas Deutz <ldeutz@roomstogo.com>
2021-04-12 09:19:48 -0400

    Add zsh config

---
+++ zsh/.zsh.d/alias.zsh
@@ -0,0 +1,19 @@
+alias ls='exa'
+alias cat='bat'
+# alias grep='rg'
+# alias find='fd'
+alias readme='glow -p README.md'
+alias vale-default='vale --config=$HOME/.vale.ini'
+
+function validate-yaml {
+  local readonly YAMLFILE="${1}"
+  ruby -ryaml -e "p YAML.load(STDIN.read)" < ${YAMLFILE}
+}
+
+function shellcheck-buildspec {
+  local readonly BUILDSPECS="${@}"
+  for spec in "${BUILDSPECS[@]}"; do
+    echo "$(tput setaf 2)$(tput bold)[+] ${spec}$(tput sgr0)"
+    yq e '.phases.[].commands.[]' "${spec}" | shellcheck --exclude=SC2148 -
+  done
+}
---
+++ zsh/.zsh.d/aws.zsh
@@ -0,0 +1,29 @@
+export AWS_DEFAULT_REGION='us-east-1'
+
+if [[ -f ~/.aws/assume-role ]]; then
+	source ~/.aws/assume-role
+fi
+
+function aws-assume-role {
+	local readonly role="$1"
+	local time="${2:-3600}"
+	if [[ "$(agp)" != "${role}" ]] then
+		( mfa 3>&1 | head -n 1 ; echo "3600" ) | acp "${role}"
+	fi
+}
+
+function s3cat {
+  local readonly S3_ARN="${1}"
+  aws s3 cp "${S3_ARN}" -
+}
+
+function s3jq {
+  local readonly S3_ARN="${1}"
+  local readonly JQ_FILTER="${2}"
+  aws s3 cp "${S3_ARN}" - | jq -r "${JQ_FILTER}"
+}
+
+function s3json {
+  local readonly S3_ARN="${1}"
+  aws s3 cp "${S3_ARN}" - | jq -r . | bat -l=json
+}
