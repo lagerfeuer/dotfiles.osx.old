176cc3f033157560c48848f5886061d75401bf40
Lukas Deutz <ldeutz@roomstogo.com>
2022-07-25 16:29:15 -0400

    Rework AWS assume role with 1Password and awsume

--- zsh/dot-zsh.d/aws.zsh
+++ zsh/dot-zsh.d/aws.zsh
@@ -1,16 +1,29 @@
 export AWS_DEFAULT_REGION='us-east-1'
 
-if [[ -f ~/.aws/assume-role ]]; then
-	source ~/.aws/assume-role
-fi
+# AWS ASSUME ROLE 
+#
+# if [[ -f ~/.aws/assume-role ]]; then
+# 	source ~/.aws/assume-role
+# fi
 
-function aws-assume-role {
-	local readonly role="$1"
-	local time="${2:-3600}"
-	if [[ "$(agp)" != "${role}" ]] then
-		( mfa | head -n 1 ; echo "${time}" ) | acp "${role}" >/dev/null
-	fi
+# function aws-assume-role {
+# 	local readonly role="$1"
+# 	local time="${2:-3600}"
+# 	if [[ "$(agp)" != "${role}" ]] then
+# 		( mfa | head -n 1 ; echo "${time}" ) | acp "${role}" >/dev/null
+# 	fi
+# }
+
+function mfa {
+  op item get AWS --vault Work --otp
 }
+
+function ar {
+  mfa | awsume $@
+}
+alias unar='awsume --unset'
+
+###############################################################################
 
 function s3cat {
   local readonly S3_ARN="${1}"
@@ -29,5 +42,10 @@ function s3json {
 }
 
 alias aws-get-id='aws sts get-caller-identity | jq .'
+alias aws-whoami='aws sts get-caller-identity | jq .'
 alias aws-roles='awk '\''/\[profile [a-z]/ { print $2 }'\'' $HOME/.aws/config |tr -d \]'
 
+function get-cf-id-for-name {
+  aws cloudfront list-distributions | \
+    jq "[.DistributionList.Items[] | { id: .Id, alias: .Aliases.Items[0] } | select(.alias == \"${1}\")]"
+}
